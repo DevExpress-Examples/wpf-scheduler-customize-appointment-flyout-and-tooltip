0682d99018c611ebe6a83785a981df5612c8e56e
AndreySlabov <andrey.slabov@devexpress.com>
2023-02-16 16:39:25 +0400

    Add Custom Fields

--- CS/CustomAppointmentFlyoutExample/MainWindow.xaml.cs
+++ CS/CustomAppointmentFlyoutExample/MainWindow.xaml.cs
@@ -1,19 +1,30 @@
-﻿using DevExpress.Xpf.Scheduling;
+﻿using DevExpress.Mvvm;
 using System;
 using System.Collections.ObjectModel;
-using System.Drawing;
-using System.Linq;
 using System.Windows;
 
 namespace CustomAppointmentFlyoutExample {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window {
         public MainWindow() {
             InitializeComponent();
-            scheduler.AppointmentItems.Add(new AppointmentItem() { Start = DateTime.Now, End = DateTime.Now.AddHours(2), LabelId = 1, StatusId = 1, Subject = "TEST" });
-            scheduler.AppointmentItems.Add(new AppointmentItem() { Start = DateTime.Now.AddHours(1), End = DateTime.Now.AddHours(3), LabelId = 1, StatusId = 1, Subject = "TEST2" });
+        }
+    }
+    public class Appointment {
+        public int ID { get; set; }
+        public DateTime? StartDate { get; set; }
+        public DateTime? EndDate { get; set; }
+        public string Subject { get; set; }
+        public int? LabelId { get; set; }
+        public string Notes { get; set; }
+    }
+    public class ViewModel : ViewModelBase {
+        public ObservableCollection<Appointment> Appointments { get; set; }
+        public ViewModel() {
+            Appointments = new ObservableCollection<Appointment> {
+                new Appointment { ID = 1, StartDate = DateTime.Now, EndDate = DateTime.Now.AddHours(1), Subject = "Nancy Davolio", LabelId = 7, Notes = "Blood test results are required" },
+                new Appointment { ID = 2, StartDate = DateTime.Now.AddHours(2), EndDate = DateTime.Now.AddHours(4), Subject = "Steven Buchanan", LabelId = 3, Notes = "Annual check-up" },
+                new Appointment { ID = 3, StartDate = DateTime.Now.AddHours(5), EndDate = DateTime.Now.AddHours(8), Subject = "Mark Oliver", LabelId = 1, Notes = "First Visit" }
+            };
         }
     }
 }
