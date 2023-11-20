3400a15adc672a6ecbf711e2041b31cb83e08aea
DevExpressExampleBot <fakeEmail>
2023-11-20 14:40:44 +0400

    Source auto update [skip ci]

--- VB/CustomAppointmentFlyoutExample/MainWindow.xaml.vb
+++ VB/CustomAppointmentFlyoutExample/MainWindow.xaml.vb
@@ -1,25 +1,18 @@
-﻿Imports DevExpress.Xpf.Scheduling
-Imports System
+Imports DevExpress.Xpf.Scheduling
 Imports System.Drawing
 Imports System.Windows
 
 Namespace CustomAppointmentFlyoutExample
+
     ''' <summary>
     ''' Interaction logic for MainWindow.xaml
     ''' </summary>
-    Partial Public Class MainWindow
+    Public Partial Class MainWindow
         Inherits Window
 
         Public Sub New()
-            InitializeComponent()
-
-            scheduler.AppointmentItems.Add(New AppointmentItem() With { _
-                .Start = Date.Now, _
-                .End = Date.Now.AddHours(2), _
-                .LabelId = 1, _
-                .StatusId = 1, _
-                .Subject = "TEST" _
-            })
+            Me.InitializeComponent()
+            Me.scheduler.AppointmentItems.Add(New AppointmentItem() With {.Start = Date.Now, .[End] = Date.Now.AddHours(2), .LabelId = 1, .StatusId = 1, .Subject = "TEST"})
         End Sub
     End Class
 End Namespace
