a6cf076f9b0c110ca13e3586a5b8459985cdff79
DevExpressExampleBot <fakeEmail>
2023-11-20 14:44:27 +0400

    Source auto update [skip ci]

--- VB/CustomAppointmentFlyoutExample/MainWindow.xaml.vb
+++ VB/CustomAppointmentFlyoutExample/MainWindow.xaml.vb
@@ -1,32 +1,19 @@
-﻿Imports DevExpress.Xpf.Scheduling
-Imports System
+Imports DevExpress.Xpf.Scheduling
 Imports System.Drawing
 Imports System.Windows
 
 Namespace CustomAppointmentFlyoutExample
+
     ''' <summary>
     ''' Interaction logic for MainWindow.xaml
     ''' </summary>
-    Partial Public Class MainWindow
+    Public Partial Class MainWindow
         Inherits Window
 
         Public Sub New()
-            InitializeComponent()
-
-            scheduler.AppointmentItems.Add(New AppointmentItem() With {
-                .Start = Date.Now,
-                .End = Date.Now.AddHours(2),
-                .LabelId = 1,
-                .StatusId = 1,
-                .Subject = "TEST"
-            })
-            scheduler.AppointmentItems.Add(New AppointmentItem() With {
-                .Start = Date.Now.AddHours(1),
-                .End = Date.Now.AddHours(3),
-                .LabelId = 1,
-                .StatusId = 1,
-                .Subject = "TEST2"
-            })
+            Me.InitializeComponent()
+            Me.scheduler.AppointmentItems.Add(New AppointmentItem() With {.Start = Date.Now, .[End] = Date.Now.AddHours(2), .LabelId = 1, .StatusId = 1, .Subject = "TEST"})
+            Me.scheduler.AppointmentItems.Add(New AppointmentItem() With {.Start = Date.Now.AddHours(1), .[End] = Date.Now.AddHours(3), .LabelId = 1, .StatusId = 1, .Subject = "TEST2"})
         End Sub
     End Class
 End Namespace
